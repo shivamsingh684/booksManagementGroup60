f103a4eec32024298ced7446e413e62f1545d303
shivamsingh684 <shivamsingh684@gmail.com>
2022-09-20 15:43:20 +0530

    validation

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const router=express.Router()
 const {createUser}=require("../controllers/userController")
+const valid=require("../validation/validation")
 
 
 
-router.post("/register",createUser)
+router.post("/register",valid.uservalidation,createUser)
 
 
 
