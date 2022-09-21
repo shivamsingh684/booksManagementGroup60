06bd39312b765de0a8ea79598fc512ce62156ed0
Manas kumar Jena <you@example.com>
2022-09-21 13:48:30 +0530

    updated validator

--- src/validation/validator.js
+++ src/validation/validator.js
@@ -49,17 +49,4 @@ let regexSpaceChar = function (attribute) {
 
 
 module.exports = { isvalidReqBody, valid,isValidTitle, isbnRegex, isREgexName, regexSpaceChar, isValidObjectId, isValidEmail, isValidPassword, moblieRegex }
-const mongoose = require('mongoose');
 
-const isValid = function (value) {
-    if(typeof value === 'undefined' || value === null) return false;
-    if(typeof value === 'string' && value.trim().length == 0) return false;
-    return true;
-}
-
-const isValidName = function (value) {
-    let nameRegex = /^[a-zA-Z ]+$/;
-    if (nameRegex.test(value)) return true;
-  };
-
-module.exports = {isValidName, isValid}
